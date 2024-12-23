f56ef08a2b493949ccded94da9bcc8c186bf2be3
Diogo Martins de Assis <diogodmda@gmail.com>
2024-12-23 20:35:44 -0300

    feat: create twitter-to-kafka-service microservices

---
+++ twitter-to-kafka-service/src/main/java/com/microservice/twitter_to_kafka_service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,11 @@
+package com.microservice.twitter_to_kafka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+	}
+}
---
+++ twitter-to-kafka-service/src/test/java/com/microservice/twitter_to_kafka_service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.microservice.twitter_to_kafka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterToKafkaServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
