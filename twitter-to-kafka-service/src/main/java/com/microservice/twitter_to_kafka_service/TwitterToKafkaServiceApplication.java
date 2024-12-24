e8cbc2bd9b3bcb3e397ec47b47a0253b3d257559
Diogo Martins de Assis <diogodmda@gmail.com>
2024-12-23 21:28:39 -0300

    feat: configuring twitter to kafka configuration and logging tracer

--- twitter-to-kafka-service/src/main/java/com/microservice/twitter_to_kafka_service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservice/twitter_to_kafka_service/TwitterToKafkaServiceApplication.java
@@ -1,11 +1,34 @@
 package com.microservice.twitter_to_kafka_service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.microservice.twitter_to_kafka_service.config.TwitterToKafkaServiceConfigData;
+
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+	private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+	private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+	public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+		this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("App started at {} {}", LocalDate.now().toString(), LocalTime.now().toString());
+		LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray()));
+		LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
 	}
 }
